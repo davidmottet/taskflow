236898e04dab8465ba1bce6ccbb57bab2a6acdb4
davidmottet <contact@davidmottet.com>
2025-01-11 19:19:57 +0100

    Refactor CustomAppBar for improved layout and consistency
    - Adjusted preferredSize to standard toolbar height for better integration.
    - Simplified padding and layout structure, enhancing maintainability.
    - Ensured consistent alignment of icons and text within the AppBar for a more cohesive UI experience.

--- lib/src/ui/app_bar_ui.dart
+++ lib/src/ui/app_bar_ui.dart
@@ -5,48 +5,47 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   const CustomAppBar({super.key});
 
   @override
-  Size get preferredSize => Size.fromHeight(kToolbarHeight * 4);
+  Size get preferredSize => Size.fromHeight(kToolbarHeight);
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(top: 32.0),
-      child: Container(
-        height: 120.0,
-        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 32.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: const [
-                    Icon(Icons.settings, color: Colors.white, size: 25.0),
-                    SizedBox(width: 8),
-                    Text('TaskFlow', style: TextStyle(color: Colors.white, fontSize: 30.0, fontWeight: FontWeight.w700)),
-                  ],
-                ),
-                Row(
-                  children: [
-                    HapticIconButton(
-                      icon: const Icon(Icons.person_2_outlined, color: Colors.white, size: 20.0),
-                      onPressed: () {
-                      },
-                    ),
-                    const SizedBox(width: 4),
-                    HapticIconButton(
-                      icon: const Icon(Icons.notifications_outlined, color: Colors.white, size: 20.0),
-                      onPressed: () {
-                      },
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ],
-        ),
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                children: const [
+                  Icon(Icons.settings, color: Colors.white, size: 25.0),
+                  SizedBox(width: 8),
+                  Text('TaskFlow', style: TextStyle(color: Colors.white, fontSize: 30.0, fontWeight: FontWeight.w700)),
+                ],
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  HapticIconButton(
+                    icon: const Icon(Icons.person_2_outlined, color: Colors.white, size: 20.0),
+                    onPressed: () {
+                    },
+                  ),
+                  const SizedBox(width: 4),
+                  HapticIconButton(
+                    icon: const Icon(Icons.notifications_outlined, color: Colors.white, size: 20.0),
+                    onPressed: () {
+                    },
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
