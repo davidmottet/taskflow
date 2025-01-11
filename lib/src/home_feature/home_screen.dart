97fb26ba521e014d2b035dd55ebd8f7cd45f573c
davidmottet <contact@davidmottet.com>
2025-01-11 17:42:16 +0100

    Refactor HomeScreen to use CustomAppBar for improved UI consistency
    - Replaced the default AppBar implementation with CustomAppBar widget.
    - Simplified the HomeScreen layout by removing redundant code related to the previous AppBar.
    - Enhanced maintainability and visual consistency across the application.

--- lib/src/home_feature/home_screen.dart
+++ lib/src/home_feature/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:taskflow/src/ui/app_bar_ui.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -8,44 +9,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: Row(
-          mainAxisSize: MainAxisSize.min,
-          children: const [
-            Icon(Icons.settings),
-            SizedBox(width: 8),
-            Text('TaskFlow'),
-          ],
-        ),
-        actions: [
-          PopupMenuButton<String>(
-            icon: const Icon(Icons.notifications),
-            onSelected: (String result) {
-              // Action à effectuer lors de la sélection d'une notification
-            },
-            itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
-              const PopupMenuItem<String>(
-                value: 'Notification 1',
-                child: Text('Notification 1'),
-              ),
-              const PopupMenuItem<String>(
-                value: 'Notification 2',
-                child: Text('Notification 2'),
-              ),
-              const PopupMenuItem<String>(
-                value: 'Notification 3',
-                child: Text('Notification 3'),
-              ),
-            ],
-          ),
-          IconButton(
-            icon: const Icon(Icons.account_circle),
-            onPressed: () {
-              // Action pour l'icône de connexion
-            },
-          ),
-        ],
-      ),
+      appBar: CustomAppBar(),
       body: const Center(
         child: Text('Welcome to the Home Screen!'),
       ),
