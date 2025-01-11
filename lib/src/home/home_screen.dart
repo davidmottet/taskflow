4d69b89da1cfc2e3b631a8aa7107a9493532fe57
davidmottet <contact@davidmottet.com>
2025-01-11 11:19:13 +0100

    Enhance HomeScreen UI: Updated AppBar to include a custom leading widget with settings icon and title, added notification popup menu, and included an account icon button for user actions.

--- lib/src/home/home_screen.dart
+++ lib/src/home/home_screen.dart
@@ -9,7 +9,42 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Home'),
+        leading: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: const [
+            Icon(Icons.settings),
+            SizedBox(width: 8),
+            Text('TaskFlow'),
+          ],
+        ),
+        actions: [
+          PopupMenuButton<String>(
+            icon: const Icon(Icons.notifications),
+            onSelected: (String result) {
+              // Action à effectuer lors de la sélection d'une notification
+            },
+            itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
+              const PopupMenuItem<String>(
+                value: 'Notification 1',
+                child: Text('Notification 1'),
+              ),
+              const PopupMenuItem<String>(
+                value: 'Notification 2',
+                child: Text('Notification 2'),
+              ),
+              const PopupMenuItem<String>(
+                value: 'Notification 3',
+                child: Text('Notification 3'),
+              ),
+            ],
+          ),
+          IconButton(
+            icon: const Icon(Icons.account_circle),
+            onPressed: () {
+              // Action pour l'icône de connexion
+            },
+          ),
+        ],
       ),
       body: const Center(
         child: Text('Welcome to the Home Screen!'),
