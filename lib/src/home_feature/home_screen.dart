0c3329a0f84407fe936475a0121d1a0efab6ace5
davidmottet <contact@davidmottet.com>
2025-01-11 19:20:11 +0100

    Enhance HomeScreen layout with new UI components
    - Added CustomSearch widget for improved search functionality.
    - Introduced HapticIconButton for user interaction with add and settings actions.
    - Updated HomeScreen to display a title and structured layout with padding and a column for better visual hierarchy.
    - Improved overall user experience by organizing UI elements effectively.

--- lib/src/home_feature/home_screen.dart
+++ lib/src/home_feature/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:taskflow/src/ui/app_bar_ui.dart';
+import 'package:taskflow/src/ui/haptic_icon_button_ui.dart';
+import 'package:taskflow/src/ui/search_ui.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -10,9 +12,35 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CustomAppBar(),
-      body: const Center(
-        child: Text('Welcome to the Home Screen!'),
-      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: Column(
+          children: [
+            const Text(
+              'Daily Work Priorities',
+              style: TextStyle(fontSize: 65.0, fontWeight: FontWeight.w500),
+            ),
+            SizedBox(height: 32.0),
+            Row(
+              children: [
+                CustomSearch(),
+                SizedBox(width: 8.0),
+                HapticIconButton(
+                  icon: Icon(Icons.add, color: Colors.white, size: 20.0),
+                  onPressed: () {
+                  },
+                ),
+                SizedBox(width: 8.0),
+                HapticIconButton(
+                  icon: Icon(Icons.settings, color: Colors.white, size: 20.0),
+                  onPressed: () {
+                  },
+                ),
+              ],
+            )
+          ],
+        ),
+      )
     );
   }
 }
