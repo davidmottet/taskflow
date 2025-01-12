64f5bc61df3a43d14163bbfbe2d5001f5d0dcc66
davidmottet <contact@davidmottet.com>
2025-01-12 16:06:57 +0100

    Add task data representation in TaskListScreen</message>
    <message>
    - Introduced a sample task data structure in TaskListScreen to display tasks dynamically.
    - Replaced the static text with a ListView.builder that utilizes the CardTask widget for rendering each task.
    - This enhancement aims to improve the user interface by providing a functional task list display.

--- lib/src/task_list_feature/task_list_screen.dart
+++ lib/src/task_list_feature/task_list_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:taskflow/src/config/theme.dart';
+import 'package:taskflow/src/ui/card_task_ui.dart';
 import 'package:taskflow/src/ui/sub_page_app_bar.dart';
 
 class TaskListScreen extends StatelessWidget {
@@ -8,6 +10,27 @@ class TaskListScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Exemple de données
+    final tasks = [
+      {
+        'color': Theme.of(context).customOrange,
+        'title': 'Réunion de projet',
+        'categories': ['Travail', 'Urgent'],
+        'startTime': '09:00 AM',
+        'endTime': '10:00 AM',
+        'lineColor': Theme.of(context).customGreen,
+      },
+      {
+        'color': Theme.of(context).customOrange,
+        'title': 'Déjeuner avec client',
+        'categories': ['Personnel'],
+        'startTime': '12:00 PM',
+        'endTime': '01:00 PM',
+        'lineColor': Theme.of(context).customViolet,
+      },
+      // Ajoutez d'autres tâches ici
+    ];
+
     return Scaffold(
       appBar: SubPageAppBar(
         pageTitle: "Task",
@@ -15,8 +38,19 @@ class TaskListScreen extends StatelessWidget {
           // Action à exécuter lorsque l'icône "+" est pressée
         }
       ),
-      body: const Center(
-        child: Text('Here is the Task List Screen!'),
+      body: ListView.builder(
+        itemCount: tasks.length, // Remplacez par le nombre d'éléments que vous avez
+        itemBuilder: (context, index) {
+          final task = tasks[index];
+          return CardTask(
+            color: task['color'] as Color,
+            lineColor: task['lineColor'] as Color,
+            title: task['title'] as String,
+            categories: task['categories'] as List<String>,
+            startTime: task['startTime'] as String,
+            endTime: task['endTime'] as String
+          );
+        }
       ),
     );
   }
