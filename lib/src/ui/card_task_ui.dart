5cdf9a37d3466ce6a083cdc1468f563fd87be470
davidmottet <contact@davidmottet.com>
2025-01-13 08:35:15 +0100

    Refactor CardTask widget to enhance layout and add new features
    - Updated the CardTask widget to improve the layout by replacing the static Row with a more flexible Wrap for category chips.
    - Introduced a new Container with a fixed width and an orange background to display a "Hello" message, aligning text to the end.
    - Adjusted the alignment of the time display to the bottom right for better visual organization.
    - These changes aim to enhance the user interface and improve the overall presentation of task details.

--- lib/src/ui/card_task_ui.dart
+++ lib/src/ui/card_task_ui.dart
@@ -47,50 +47,51 @@ class CardTask extends StatelessWidget {
                     title,
                     style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Theme.of(context).primaryColor),
                   ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      Wrap(
-                        spacing: 2.0,
-                        children: categories.map((category) {
-                          return Chip(
-                            label: Text(
-                              category,
-                              style: TextStyle(fontSize: 8, color: Theme.of(context).colorScheme.primary),
-                            ),
-                            backgroundColor: Color(0xFFfee5a0),
-                            padding: EdgeInsets.symmetric(horizontal: 0, vertical: 0),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(16.0),
-                              side: BorderSide(color: Colors.transparent),
-                            ),
-                          );
-                        }).toList(),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(bottom: 8.0),
-                        child: Row(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            Text(
-                              startTime,
-                              style: TextStyle(fontSize: 12, color: Theme.of(context).primaryColor),
-                            ),
-                            const SizedBox(width: 4.0),
-                            Text(
-                              '-',
-                              style: TextStyle(fontSize: 12, color: Theme.of(context).primaryColor)
-                            ),
-                            const SizedBox(width: 4.0),
-                            Text(
-                              endTime,
-                              style: TextStyle(fontSize: 12, color: Theme.of(context).primaryColor),
-                            ),
-                          ],
+                  Wrap(
+                    spacing: 2.0,
+                    children: categories.map((category) {
+                      return Chip(
+                        label: Text(
+                          category,
+                          style: TextStyle(fontSize: 8, color: Theme.of(context).colorScheme.primary),
                         ),
-                      )
-                    ],
+                        backgroundColor: Color(0xFFfee5a0),
+                        padding: EdgeInsets.symmetric(horizontal: 0, vertical: 0),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(16.0),
+                          side: BorderSide(color: Colors.transparent),
+                        ),
+                      );
+                    }).toList(),
+                  ),
+                  Container(
+  width: 200, // Ensure the container has enough width
+  color: Colors.orange,
+  child: Text(
+    "Hello",
+    textAlign: TextAlign.end, // Aligns the text to the end of the container
+  ),
+),
+                  Align(
+                    alignment: Alignment.bottomRight,
+                    child: Row(
+                      children: [
+                        Text(
+                          startTime,
+                          style: TextStyle(fontSize: 12, color: Theme.of(context).primaryColor),
+                        ),
+                        const SizedBox(width: 4.0),
+                        Text(
+                          '-',
+                          style: TextStyle(fontSize: 12, color: Theme.of(context).primaryColor)
+                        ),
+                        const SizedBox(width: 4.0),
+                        Text(
+                          endTime,
+                          style: TextStyle(fontSize: 12, color: Theme.of(context).primaryColor),
+                        ),
+                      ],
+                    ),
                   )
                 ],
               )
